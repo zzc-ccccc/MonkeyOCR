e7c99f23caf55f2e0463c5da98fe87680077288a
echo840 <87795401+echo840@users.noreply.github.com>
2025-06-09 17:56:53 +0800

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,7 +1,5 @@
-from huggingface_hub import snapshot_download
 from argparse import ArgumentParser
 import os
-from modelscope import snapshot_download as modelscope_download
 
 
 if __name__ == '__main__':
@@ -11,6 +9,8 @@ if __name__ == '__main__':
     if not os.path.exists("./model_weight"):
         os.makedirs("./model_weight")
     if args.type == "huggingface":
+        from huggingface_hub import snapshot_download
         snapshot_download(repo_id="echo840/MonkeyOCR",local_dir="./model_weight",local_dir_use_symlinks=False, resume_download=True)
     elif args.type == "modelscope":
+        from modelscope import snapshot_download as modelscope_download
         modelscope_download(repo_id = 'l1731396519/MonkeyOCR',local_dir="./model_weight")
