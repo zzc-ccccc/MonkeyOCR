b2f20b0d590a198bae78d9f14c87633f6bec616b
Dai Wenzhuo <rockdai321@gmail.com>
2025-06-12 15:33:01 +0800

    Update download_model.py, fix ModelScope download script. (#37)

--- tools/download_model.py
+++ tools/download_model.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
         snapshot_download(repo_id="echo840/MonkeyOCR", local_dir=model_dir, local_dir_use_symlinks=False, resume_download=True)
     elif args.type == "modelscope":
         from modelscope import snapshot_download as modelscope_download
-        modelscope_download(repo_id = 'ByteDance/Dolphin',local_dir=model_dir)
+        modelscope_download(repo_id = 'l1731396519/MonkeyOCR',local_dir=model_dir)
